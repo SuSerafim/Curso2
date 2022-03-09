e5cf103379ae581b969f0bf9f9550af2c16aaa85
Suelen Serafim <s.s.serafim@outlook.com>
2022-03-09 16:28:24 +0000

    Add project files.

---
+++ Curso2/Entities/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace Curso2.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; } /*atributo*/
+        public int Hours { get; set; }  /*atributo*/
+        public double ValuePerHour { get; set; }  /*atributo*/
+
+        //Construtor padrão
+        public Employee()
+        {
+
+        }
+
+        //Construtor com parametros
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+       
+        //Metodo
+        public virtual double Payment() /* liberando o metodo para que seja sobrescrito*/
+        {
+            return Hours * ValuePerHour;
+        }
+
+    }
+}
---
+++ Curso2/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso2.Entities
+{
+    class OutsourcedEmployee : Employee //herda a classe Employee (todas as propriedades e metodos)
+    {
+        public double AdditionalCharge { get; set; }//atributo
+
+        //Construtor padrão
+        public OutsourcedEmployee()
+        {
+
+        }
+
+        //Construtor com parametros
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) 
+            : base (name, hours, valuePerHour ) /*reaproveitando os argumentos */
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+
+        //Fazendo uma sobreposição do metodo Payment existente
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge; /* reaproveitando a lógica do Payment comum + a despesa adicional*/
+        }
+    }
+}
